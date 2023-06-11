6507ede8b80e831fe8d799af92108bd1aa5da975
Jorge Henrique <jorgehenriquedasilva2014@hotmail.com>
2023-06-11 12:08:13 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jorge Henrique on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
